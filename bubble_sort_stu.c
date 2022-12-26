560a924267753fd287964ebea5996877de4aa08f
HaticeSerin <119755031+HaticeSerin@users.noreply.github.com>
2022-12-26 10:07:08 +0300

    Hatice Serin
    @themanoftalent

--- bubble_sort_stu.c
+++ bubble_sort_stu.c
@@ -1,2 +1,22 @@
 // put in order 96 85 1 2 34 5 45 74 25 12 6
 
+#include"stdio.h"
+
+int main(){
+	int i,j,temp;
+	int arr[11]={96,85,1,2,34,5,45,74,25,12,6};
+	
+	for(i=0;i<11;i++){
+		for(j=0;j<11;j++){
+			if(arr[i]<arr[j]){
+				temp=arr[i];
+				arr[i]=arr[j];
+				arr[j]=temp;
+			}
+		}
+	}
+	for(i=0;i<11;i++){
+		printf("%d ",arr[i]);
+	}
+	return 0;
+}
