bcf7a5da93e70de5f24719e777e81f313cb02d2b
muhammedemreagda <119903051+muhammedemreagda@users.noreply.github.com>
2022-12-26 10:10:48 +0300

    Update bubble_sort_stu.c

--- bubble_sort_stu.c
+++ bubble_sort_stu.c
@@ -1,25 +1,23 @@
-// put in order 96 85 1 2 34 5 45 74 25 12 6
-
-#include "stdlib.h"
-#include "stdio.h"
-
-int main() {
-    int i,j,temp;
-    int dizi[11] = {96,85,1,2,34,5,45,74,25,12,6};
-    
-    for(i=0; i<11; i++) {
-        for(j=0; j<11; j++) {
-            if(dizi[i] < dizi[j]) {
-                temp = dizi[i];
-                dizi[i] = dizi[j];
-                dizi[j] = temp;
-            }
-        }
-    }
-    
-    for(i=0; i<11; i++) {
-        printf("%d ",dizi[i]);
-    }
-    
-    return 0;
-}
+ #include <stdio.h>
+  // put in order 96 85 1 2 34 5 45 74 25 12 6
+ int main() {
+ 
+ 	int sayilar[11],i,j,gecici; 
+ 	for(i=0; i<11; i++) { 
+ 		printf("Sayi giriniz:"); 
+ 		scanf("%d",&sayilar[i]);
+ 	}
+ 	for(i=0; i <11; i++) { 
+ 		for(j=0; j<11; j++) {
+ 			if(sayilar[i]<sayilar[j]) { 
+ 				gecici = sayilar[i]; 
+ 				sayilar[i] = sayilar[j]; 
+ 				sayilar[j] = gecici; 
+ 			}
+ 		}
+ 	}
+ 	for(i=0; i<5; i++) {
+ 		printf("%d ",sayilar[i]);
+ 	}
+ 	return 0;
+ }
