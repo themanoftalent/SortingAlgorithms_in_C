3be8e1b535e490c912e8523847c044bf9b2fe18c
HaticeSerin <119755031+HaticeSerin@users.noreply.github.com>
2022-12-27 13:37:03 +0300

    Hatice Serin

--- Alphabettt.c
+++ Alphabettt.c
@@ -1,16 +1,16 @@
 #include"stdio.h"
  int main(){
  	int i,j,k;
- 	char alfabet='a',n;
+ 	char alphabet='a',n;
  	printf("En son hangi Harf olsun? ");
  	scanf("%c",&n);
  	
  	for(i=0;i<=(n-'a');i++){
  		for(j=0;j<=i;j++){
- 			printf("%c",alfabet);
+ 			printf("%c",alphabet);
 		 }
  		printf("\n");
- 		alfabet++;
+ 		alphabet++;
 	 }
 	 return 0;
  }
