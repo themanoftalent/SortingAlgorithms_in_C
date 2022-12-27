a9e6f72f58ee97dc56400d17ddc4886e710ae376
HaticeSerin <119755031+HaticeSerin@users.noreply.github.com>
2022-12-27 13:25:20 +0300

    Hatice Serin

---
+++ terspiramit.c
@@ -0,0 +1,14 @@
+#include"stdio.h"
+ int main(){
+ 	int i,j,n,k;
+ 	printf("Kac satir olsun? ");
+ 	scanf("%d",&n);
+ 	
+ 	for(i=n;i>=1;i--){
+ 		for(j=1;j<=i;j++){
+ 			printf("%d",j);
+		 }
+ 		printf("\n");
+	 }
+	 return 0;
+ }
